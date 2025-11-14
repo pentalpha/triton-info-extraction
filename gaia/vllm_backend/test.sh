f6e606d2b9a008d0da89f2cfb6c536f178deb6c4
Pitágoras Alves <alves.pitagoras@gmail.com>
2025-11-14 16:41:16 -0300

    Changed from generic name to gaia

--- gaia/vllm_backend/test.sh
+++ gaia/vllm_backend/test.sh
@@ -1,6 +1,6 @@
 # Make sure your my_model_repo is in your current directory
 export TRITON_SERVER_URL=0.0.0.0:8000
-export LLM_NAME=text_llm3
+export LLM_NAME=gaia
 time curl -X POST $TRITON_SERVER_URL/v2/models/$LLM_NAME/generate_stream -d \
 '{
   "text_input": "Dê uma descrição curta sobre a cidade de São Paulo.",
